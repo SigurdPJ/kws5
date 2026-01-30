4962034769d94f6069cffac9db9d7070481d73d2
Sigurd Jongers <sijo063@student.kristiania.no>
2026-01-30 23:35:07 +0100

    kartverket map

--- src/components/app/application.tsx
+++ src/components/app/application.tsx
@@ -1,37 +1,41 @@
 import React, { useEffect, useRef } from "react";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile.js";
-import { OSM } from "ol/source.js";
+import { WMTS } from "ol/source.js";
 import { useGeographic } from "ol/proj.js";
 
-// Styling of OpenLayers components like zoom and pan controls
 // @ts-ignore
 import "ol/ol.css";
+import { WMTSCapabilities } from "ol/format.js";
+import { optionsFromCapabilities } from "ol/source/WMTS.js";
 
-// By calling the "useGeographic" function in OpenLayers, we tell that we want coordinates to be in degrees
-//  instead of meters, which is the default. Without this `center: [10.6, 59.9]` brings us to "null island"
 useGeographic();
 
-// Here we create a Map object. Make sure you `import { Map } from "ol"`. Otherwise, the standard Javascript
-//  map data structure will be used
-const map = new Map({
-  // The map will be centered on a position in longitude (x-coordinate, east) and latitude (y-coordinate, north),
-  //   with a certain zoom level
-  view: new View({ center: [10.8, 59.9], zoom: 13 }),
-  // map tile images will be from the Open Street Map (OSM) tile layer
-  layers: [new TileLayer({ source: new OSM() })],
+const kartverket = new TileLayer({});
+const kartverketUrl =
+  "https://cache.kartverket.no/v1/wmts/1.0.0/WMTSCapabilities.xml";
+fetch(kartverketUrl).then(async (response) => {
+  const parser = new WMTSCapabilities();
+  kartverket.setSource(
+    new WMTS(
+      optionsFromCapabilities(parser.read(await response.text()), {
+        layer: "toporaster",
+        matrixSet: "webmercator",
+      })!,
+    ),
+  );
 });
 
-// A functional React component
+const map = new Map({
+  view: new View({ center: [10.8, 59.9], zoom: 13 }),
+  layers: [kartverket],
+});
+
 export function Application() {
-  // `useRef` bridges the gap between JavaScript functions that expect DOM objects and React components
   const mapRef = useRef<HTMLDivElement | null>(null);
-  // When we display the page, we want the OpenLayers map object to target the DOM object refererred to by the
-  // map React component
   useEffect(() => {
     map.setTarget(mapRef.current!);
   }, []);
 
-  // This is the location (in React) where we want the map to be displayed
   return <div ref={mapRef}></div>;
 }
